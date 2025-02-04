d7c0b4e24a6d5a1773fed944a1939dbf5a3f6e0c
baxterc5 <131222241+baxterc5@users.noreply.github.com>
2025-02-04 03:07:28 -0500

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,7 @@
 module.exports = function(eleventyConfig) {
-  // Ensure images and styles are copied to the output folder
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
 
-  // Shortcode for dynamic year
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 
   return {
